28ee9c22f78de4f79a9614ad4d8e2361cfde0653
mechamogera <mechamosura@gmail.com>
2013-12-04 10:31:53 +0900

    support ssh options for rsync

--- lib/capistrano/recipes/deploy/strategy/rsync_with_remote_cache.rb
+++ lib/capistrano/recipes/deploy/strategy/rsync_with_remote_cache.rb
@@ -44,7 +44,9 @@ module Capistrano
         end
         
         def rsync_command_for(server)
-          "rsync #{rsync_options} --rsh='ssh -p #{ssh_port(server)}' #{local_cache_path}/ #{rsync_host(server)}:#{repository_cache_path}/"
+          ssh_options_str = "-p #{ssh_port(server)} "
+          ssh_options_str << ssh_options[:keys].inject("") { |x, key| x += "-i #{key} "; x } if ssh_options[:keys]
+          "rsync #{rsync_options} --rsh='ssh #{ssh_options_str} #{rsync_ssh_options}' #{local_cache_path}/ #{rsync_host(server)}:#{repository_cache_path}/"
         end
         
         def mark_local_cache
--- lib/capistrano_rsync_with_remote_cache/version.rb
+++ lib/capistrano_rsync_with_remote_cache/version.rb
@@ -1,3 +1,3 @@
 module CapistranoRsyncWithRemoteCache
-  VERSION = "2.4.0"
+  VERSION = "2.4.1"
 end
